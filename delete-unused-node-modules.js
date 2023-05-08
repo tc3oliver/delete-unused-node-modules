567eaf822ec1d6ff49df6d230a6e6e3e48d7fad6
Nick.yu <nick.yu@hzn.com.tw>
2023-05-08 12:18:47 +0800

    refactor: refactor code for improved readability and compatibility
    - Change single quotes to double quotes in the `require` statements
    - Remove semicolons at the end of lines
    - Replace arrow function with normal function syntax for compatibility with older Node.js versions
    - Add comments for better readability
    
    Signed-off-by: Nick.yu <nick.yu@hzn.com.tw>

--- delete-unused-node-modules.js
+++ delete-unused-node-modules.js
@@ -1,78 +1,81 @@
-const fs = require("fs");
-const path = require("path");
-const cheerio = require("cheerio");
+const fs = require('fs')
+const path = require('path')
+const cheerio = require('cheerio')
 
-const projectRoot = "./";
+const projectRoot = './'
 
 // 遞歸遍歷目錄，找到所有 HTML 文件
 function walk(dir) {
-  let files = fs.readdirSync(dir);
+  let files = fs.readdirSync(dir)
   files.forEach((file) => {
-    let filePath = path.join(dir, file);
-    let stat = fs.statSync(filePath);
+    let filePath = path.join(dir, file)
+    let stat = fs.statSync(filePath)
     if (stat.isDirectory()) {
-      walk(filePath);
-    } else if (path.extname(file) === ".html") {
-      processHtmlFile(filePath);
+      walk(filePath)
+    } else if (path.extname(file) === '.html') {
+      processHtmlFile(filePath)
     }
-  });
+  })
 }
 
 // 處理每個 HTML 文件，提取 script 標籤和 link 標籤中的路徑
 function processHtmlFile(filePath) {
-  let html = fs.readFileSync(filePath, "utf8");
-  let $ = cheerio.load(html);
-  $("script[src], link[href]").each((i, el) => {
-    let url = $(el).attr("src") || $(el).attr("href");
-    if (url && url.includes("node_modules")) {
-      let importPath = path.join(projectRoot, url);
+  let html = fs.readFileSync(filePath, 'utf8')
+  let $ = cheerio.load(html)
+  $('script[src], link[href]').each((i, el) => {
+    let url = $(el).attr('src') || $(el).attr('href')
+    if (url && url.includes('node_modules')) {
+      let importPath = path.join(projectRoot, url)
+
+      // 將路徑轉換為絕對路徑，方便後面進行比對
+      const absolutePath = path.resolve(url)
 
       // 使用正則表達式匹配路徑中 node_modules 的位置
-    const match = new RegExp(`node_modules${path.sep}(.*)`).exec(importPath);
+      const match = /node_modules[\\/](.*)/.exec(absolutePath)
 
-    if (match) {
-      // 如果匹配成功，截斷前面的路徑部分，只保留 node_modules 及其後面的路徑
-      const nodeModulesPath = match[0];
-      console.log(nodeModulesPath);
+      if (match) {
+        // 如果匹配成功，截斷前面的路徑部分，只保留 node_modules 及其後面的路徑
+        const nodeModulesPath = match[0]
+        console.log(nodeModulesPath)
 
-      // 將符合條件的路徑存儲到 paths 陣列中
-      paths.push(nodeModulesPath);
-    } else {
-      // 如果匹配失敗，表示路徑中不包含 node_modules
-      console.log("路徑中不包含 node_modules");
+        // 將符合條件的路徑存儲到 paths 陣列中
+        paths.push(nodeModulesPath)
+      } else {
+        // 如果匹配失敗，表示路徑中不包含 node_modules
+        console.log('路徑中不包含 node_modules')
+      }
+      console.log(importPath)
     }
-      console.log(importPath);
-    }
-  });
+  })
 }
 
 // 遍歷專案根目錄，找到所有 HTML 文件並處理
-let paths = [];
-walk(projectRoot);
+let paths = []
+walk(projectRoot)
 
-console.log(paths);
+console.log(paths)
 
 // 遍歷 node_modules 目錄下的所有文件，刪除未被依賴的文件
-const nodeModulesDir = 'node_modules';
+const nodeModulesDir = 'node_modules'
 
 function deleteNM(dir) {
-  let files = fs.readdirSync(dir);
-  files.forEach(file => {
-    let filePath = path.join(dir, file);
-    let stat = fs.statSync(filePath);
+  let files = fs.readdirSync(dir)
+  files.forEach((file) => {
+    let filePath = path.join(dir, file)
+    let stat = fs.statSync(filePath)
     if (stat.isDirectory()) {
-      deleteNM(filePath);
+      deleteNM(filePath)
     } else {
       // 判斷文件是否被依賴
       if (!usedPaths.includes(filePath)) {
         // 如果未被依賴，刪除文件
-        fs.unlinkSync(filePath);
-        console.log(`已刪除文件：${filePath}`);
+        fs.unlinkSync(filePath)
+        console.log(`已刪除文件：${filePath}`)
       }
     }
-  });
+  })
 }
 
 // 遞歸遍歷 node_modules 目錄下的所有文件並刪除未被依賴的文件
-const usedPaths = paths;
-deleteNM(nodeModulesDir);
+const usedPaths = paths
+deleteNM(nodeModulesDir)

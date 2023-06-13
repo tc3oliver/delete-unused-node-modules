f95e95e09e702285730058f3356917de12126abf
tc3oliver <57586652+tc3oliver@users.noreply.github.com>
2023-06-13 14:04:10 +0800

    Update delete-unused-node-modules.js

--- delete-unused-node-modules.js
+++ delete-unused-node-modules.js
@@ -19,88 +19,76 @@ function walk(dir) {
 
 // 處理每個 HTML 檔案，提取 script 標籤和 link 標籤中的路徑
 function processHtmlFile(filePath) {
-  let html = fs.readFileSync(filePath, "utf8");
-  let scriptUrls = html.matchAll(/<script.+src=["']([^"']*)["']/gi);
-  let linkUrls = html.matchAll(/<link.+href=["']([^"']*)["']/gi);
+  const html = fs.readFileSync(filePath, 'utf8')
 
-  for (let match of scriptUrls) {
-    let url = match[1];
-    if (url && url.includes("node_modules")) {
-      let importPath = path.join(projectRoot, url);
-
-      // 將路徑轉換為絕對路徑，方便後面進行比對
-      const absolutePath = path.resolve(url);
-
-      // 使用正則表達式匹配路徑中 node_modules 的位置
-      const match = /node_modules[\\/](.*)/.exec(absolutePath);
-
+  const scriptRegex = /<script[^>]*src=["']([^"']+)["'][^>]*>\s*<\/script>/g
+  let match
+  while ((match = scriptRegex.exec(html)) !== null) {
+    const url = match[1]
+    if (url && url.includes('node_modules')) {
+      const importPath = path.join(projectRoot, url)
+      const absolutePath = path.resolve(url)
+      const match = /node_modules[\\/](.*)/.exec(absolutePath)
       if (match) {
-        // 如果匹配成功，截斷前面的路徑部分，只保留 node_modules 及其後面的路徑
-        const nodeModulesPath = match[0];
-        console.log(nodeModulesPath);
-
-        // 將符合條件的路徑存儲到 paths 陣列中
-        paths.push(nodeModulesPath);
+        const nodeModulesPath = match[0]
+        console.log(nodeModulesPath)
+        paths.push(nodeModulesPath)
       } else {
-        // 如果匹配失敗，表示路徑中不包含 node_modules
-        console.log("路徑中不包含 node_modules");
+        console.log('路徑中不包含 node_modules')
       }
-      console.log(importPath);
+      console.log(importPath)
     }
   }
 
-  for (let match of linkUrls) {
-    let url = match[1];
-    if (url && url.includes("node_modules")) {
-      let importPath = path.join(projectRoot, url);
-
-      // 將路徑轉換為絕對路徑，方便後面進行比對
-      const absolutePath = path.resolve(url);
-
-      // 使用正則表達式匹配路徑中 node_modules 的位置
-      const match = /node_modules[\\/](.*)/.exec(absolutePath);
-
+  const linkRegex = /<link[^>]*href=["']([^"']+)["'][^>]*>/g
+  while ((match = linkRegex.exec(html)) !== null) {
+    const url = match[1]
+    if (url && url.includes('node_modules')) {
+      const importPath = path.join(projectRoot, url)
+      const absolutePath = path.resolve(url)
+      const match = /node_modules[\\/](.*)/.exec(absolutePath)
       if (match) {
-        // 如果匹配成功，截斷前面的路徑部分，只保留 node_modules 及其後面的路徑
-        const nodeModulesPath = match[0];
-        console.log(nodeModulesPath);
-
-        // 將符合條件的路徑存儲到 paths 陣列中
-        paths.push(nodeModulesPath);
+        const nodeModulesPath = match[0]
+        console.log(nodeModulesPath)
+        paths.push(nodeModulesPath)
       } else {
-        // 如果匹配失敗，表示路徑中不包含 node_modules
-        console.log("路徑中不包含 node_modules");
+        console.log('路徑中不包含 node_modules')
       }
-      console.log(importPath);
+      console.log(importPath)
     }
   }
 }
+
 
 // 遍歷專案根目錄，找到所有 HTML 文件並處理
 let paths = [];
 walk(projectRoot);
 
-console.log(paths);
 
 // 遍歷 node_modules 目錄下的所有文件，刪除未被依賴的文件
-const nodeModulesDir = "node_modules";
+const nodeModulesDir = 'node_modules'
+const excludedPath = 'icons-webfont'
 
 function deleteNM(dir) {
-  let files = fs.readdirSync(dir);
+  let files = fs.readdirSync(dir)
   files.forEach((file) => {
-    let filePath = path.join(dir, file);
-    let stat = fs.statSync(filePath);
+    let filePath = path.join(dir, file)
+    let stat = fs.statSync(filePath)
     if (stat.isDirectory()) {
-      deleteNM(filePath);
+      // 檢查當前目錄路徑是否包含被排除的字串
+      if (filePath.includes(excludedPath)) {
+        return
+      }
+      deleteNM(filePath)
     } else {
-      // 判斷文件是否被依賴
-      if (!usedPaths.includes(filePath)) {
-        // 如果未被依賴，刪除文件
-        fs.unlinkSync(filePath);
-        console.log(`已刪除文件：${filePath}`);
+      // 判斷檔案路徑是否包含被排除的字串
+      if (!filePath.includes(excludedPath) && !usedPaths.includes(filePath)) {
+        // 如果未被依賴，刪除檔案
+        fs.unlinkSync(filePath)
+        console.log(`已刪除檔案：${filePath}`)
       }
     }
-  });
+  })
 }
 
 // 遞歸遍歷 node_modules 目錄下的所有文件並刪除未被依賴的文件
